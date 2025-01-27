7142a00dfb46c169264516cd69e8d295c7f6f5e0
Tim Van Dyck <tim.van.dyck@davinsi.com>
2025-01-27 22:15:51 +0100

    feat: limit result json with only necessary fields. Also give preference to english if there are duplicates

--- combine_json.py
+++ combine_json.py
@@ -3,6 +3,9 @@ import json
 from datetime import datetime
 from override_list import override_list
 
+def extract_language(url):
+    return url.split('//')[1].split('.')[0]
+
 def combine_json_files(directory):
     combined_data = {}
 
@@ -16,18 +19,46 @@ def combine_json_files(directory):
             for entry in data:
                 date = entry['date']
                 if date not in combined_data:
-                    combined_data[date] = entry
-                    combined_data[date]['data']['deaths'] = [
-                        death for death in entry['data']['deaths'] 
-                        if all(page['title'] not in override_list for page in death['pages'])
-                    ]
+                    combined_data[date] = {
+                        'date': date,
+                        'data': {
+                            'deaths': [
+                                {
+                                    'text': death['text'],
+                                    'title': death['pages'][0]['title'],
+                                    'description': death['pages'][0].get('description', ''),
+                                    'content_url': death['pages'][0]['content_urls']['desktop']['page'],
+                                    'language': extract_language(death['pages'][0]['content_urls']['desktop']['page']),
+                                    'extract': death['pages'][0].get('extract', '')
+                                }
+                                for death in entry['data']['deaths']
+                                if all(page['title'] not in override_list for page in death['pages'])
+                            ]
+                        }
+                    }
                 else:
-                    existing_titles = {page['title'] for death in combined_data[date]['data']['deaths'] for page in death['pages']}
+                    existing_titles = {death['title'] for death in combined_data[date]['data']['deaths']}
                     new_deaths = [
-                        death for death in entry['data']['deaths'] 
+                        {
+                            'text': death['text'],
+                            'title': death['pages'][0]['title'],
+                            'description': death['pages'][0].get('description', ''),
+                            'content_url': death['pages'][0]['content_urls']['desktop']['page'],
+                            'language': extract_language(death['pages'][0]['content_urls']['desktop']['page']),
+                            'extract': death['pages'][0].get('extract', '')
+                        }
+                        for death in entry['data']['deaths']
                         if all(page['title'] not in existing_titles and page['title'] not in override_list for page in death['pages'])
                     ]
-                    combined_data[date]['data']['deaths'].extend(new_deaths)
+                    for new_death in new_deaths:
+                        # Check if the title already exists and if the new entry is in English
+                        existing_death = next((d for d in combined_data[date]['data']['deaths'] if d['title'] == new_death['title']), None)
+                        if existing_death:
+                            if new_death['language'] == 'en':
+                                combined_data[date]['data']['deaths'].remove(existing_death)
+                                combined_data[date]['data']['deaths'].append(new_death)
+                        else:
+                            combined_data[date]['data']['deaths'].append(new_death)
 
     # Convert combined_data to a list and sort by date
     combined_data_list = list(combined_data.values())
